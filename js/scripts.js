4f4eb25882daa70ebc3c21957ffa4c9d1cc17b0c
Frankie Gavin <frankanthonygavin@gmail.com>
2017-01-06 09:29:43 -0800

    add html with form input, js prompt for ailment with if else statement.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+var askAilment = prompt("What is your ailment?")
+
+var toothache = true;
+    if (toothache) {
+    document.write("You should see Dr. Smith")
+    } else  {
+      document.write("No doctors found.")
+  }
+
+// $(document).ready(function() {
+//   $('#aliment-form').submit(function(event) {
+//     event.preventDefault();
+//     var goal = $('#goal').val();
+//     var output = pingPong(goal);
+//     output.forEach(function(element) {
+//       $('#solution').append("<li>" + element + "</li>");
+//     });
+//   });
+// });
